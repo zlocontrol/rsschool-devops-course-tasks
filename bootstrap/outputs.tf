31f4bfd5742efe382761f7cc4b4af6d09e947a8c
Igor <rukavitsyn.i.v@gmail.com>
2025-06-15 20:18:39 +0300

    Add bootstrap workflow

---
+++ bootstrap/outputs.tf
@@ -0,0 +1,9 @@
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_state.id
+}
+
+
+output "github_role_arn" {
+  description = "ARN of the GitHub OIDC IAM role"
+  value       = aws_iam_role.github_actions_role.arn
+}
---
+++ bootstrap/variables.tf
@@ -0,0 +1,58 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+
+}
+
+variable "role_name" {
+  description = "Role IAM"
+  type        = string
+}
+
+
+variable "github_repo_owner" {
+  description = "Owner (organization or user) GitHub-репозитория для OIDC."
+  type        = string
+  default     = "zlocontrol"
+}
+
+variable "github_repo_name" {
+  description = "The name of the GitHub repository for OIDC."
+  type        = string
+  default     = "rsschool-devops-course-tasks" #
+}
+
+
+variable "project_name" {
+  description = "Project name (for tags)"
+  type        = string
+  default     = "project*"
+}
+
+#name bucket
+variable "s3_bucket_name" {
+  description = "Base name for the S3 bucket to store Terraform state"
+  type        = string
+}
+
+# general tag (словарь key = value)
+variable "common_tags" {
+  description = "A map of tags to apply to resources"
+  type        = map(string)
+  default = {
+    Owner     = "your-name"
+    ManagedBy = "Terraform"
+    Project   = "DevOps Course"
+  }
+}
+
+variable "environment" {
+  description = "Environment (eg dev, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "owner" {
+  description = "Owner of the infrastructure"
+  type        = string
+  default     = "devops-team"
+}
