0b7614ea7914c64d261904c9a3490e4e4197c414
Igor <rukavitsyn.i.v@gmail.com>
2025-06-15 21:56:07 +0300

    Add bootstrap workflow 03

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,9 +1,11 @@
 output "s3_bucket_name" {
   value = aws_s3_bucket.terraform_state.id
+  sensitive = true
 }
 
 
 output "github_role_arn" {
   description = "ARN of the GitHub OIDC IAM role"
   value       = aws_iam_role.github_actions_role.arn
+  sensitive = true
 }
