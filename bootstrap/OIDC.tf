8d23ba782ef85d0c7505e6129bfe2da02ba2f112
Igor <rukavitsyn.i.v@gmail.com>
2025-06-16 22:40:55 +0300

    Add-bootstrap-01

--- bootstrap/OIDC.tf
+++ bootstrap/OIDC.tf
@@ -1,23 +1,23 @@
-# Get the current AWS account
 data "aws_caller_identity" "current" {}
 
-# Trying to find an existing GitHub OIDC provider
+# Trying to find an existing OIDC provider - do not use directly!
 data "aws_iam_openid_connect_provider" "existing" {
   url = "https://token.actions.githubusercontent.com"
 }
 
-# Create only if not found
+# Create OIDC provider only if not found
 resource "aws_iam_openid_connect_provider" "github" {
-  count = can(data.aws_iam_openid_connect_provider.existing.arn) ? 0 : 1
+  count = try(data.aws_iam_openid_connect_provider.existing.arn, null) != null ? 0 : 1
 
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780fa86"]
 }
 
-# Local variable with ARN
+# Use local variable with ARN
 locals {
-  oidc_provider_arn = can(data.aws_iam_openid_connect_provider.existing.arn) ? data.aws_iam_openid_connect_provider.existing.arn : aws_iam_openid_connect_provider.github[0].arn
+  maybe_existing     = try(data.aws_iam_openid_connect_provider.existing.arn, null)
+  oidc_provider_arn  = local.maybe_existing != null ? local.maybe_existing : aws_iam_openid_connect_provider.github[0].arn
 
   policies = [
     "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
@@ -29,6 +29,7 @@ locals {
     "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess",
   ]
 }
+
 
 # # 2. Create an IAM role that GitHub Actions will assume
 # # This role will be used by GitHub Actions to obtain temporary AWS credentials.
