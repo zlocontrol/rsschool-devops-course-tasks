83e2ee57240e45486e5cf7f4f2dd28c3180c3de9
Igor <rukavitsyn.i.v@gmail.com>
2025-07-21 15:40:03 +0300

    fix_sonar_10

--- flask-app/tests/test_app.py
+++ flask-app/tests/test_app.py
@@ -1,22 +1,18 @@
+
 import pytest
-from main import app
+from flask_app import app
 
 @pytest.fixture
 def client():
-
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 def test_index_page(client):
-
     response = client.get('/')
     assert response.status_code == 200
     assert b"Hello, Flask!" in response.data
 
 def test_nonexistent_page(client):
-
     response = client.get('/nonexistent-page')
     assert response.status_code == 404
-    assert b"Not Found" in response.data or b"The requested URL was not found on the server." in response.data
-
