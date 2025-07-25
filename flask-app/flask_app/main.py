bdb22c0c0b601096924fdaeafc07eedd1af48656
Igor <rukavitsyn.i.v@gmail.com>
2025-07-25 17:27:39 +0300

    moni_01

--- flask-app/flask_app/main.py
+++ flask-app/flask_app/main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+# Создаём счётчик запросов
+REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests to the root endpoint')
+
 @app.route("/")
 def index():
+    REQUEST_COUNT.inc()  # Увеличиваем счётчик каждый раз, когда вызывается /
     return "Hello, Flask!"
+
+# Новый endpoint — метрики Prometheus
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
